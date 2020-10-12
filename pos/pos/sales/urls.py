8a9684fc388a1518689a0cc02038dc282bc06e37
kharis12-web <brayensoulty22@gmail.com>
2020-10-12 11:06:10 +0700

    pagi

---
+++ pos/pos/sales/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('', views.index),
+	path('transaksi/', views.transaksi),
+	path('input/', views.input),
+	path('print/', views.cetak),
+	path('<id>/', views.sale_detail),
+	path('<id>/delete', views.delete),
+	# path('<id>/update', views.update),
+	path('<id>/detail', views.detail),
+	path('<id>/detail/<id_detail>/delete', views.delete_detail),
+]
